9c285e92803a4c9d11de97c0c33e2361134d86f2
Rakuto-NOV <okudarakuto@gmail.com>
2022-05-26 21:35:36 +0900

    チャットルームの削除機能の実装

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,10 +1,9 @@
 class RoomsController < ApplicationController
 
+ def index
 
-  def index
   end
 
-  
   def new
     @room = Room.new
   end
@@ -16,6 +15,12 @@ class RoomsController < ApplicationController
     else
       render :new
     end
+  end
+
+  def destroy
+    room = Room.find(params[:id])
+    room.destroy
+    redirect_to root_path
   end
 
   private
